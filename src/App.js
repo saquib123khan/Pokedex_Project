3cc41a50283f7869a771872585aecc67e826a31c
Saquib khan <saquibkhan122335@gmail.com>
2024-06-30 13:46:57 +0530

    basic pokedex setup done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Pokedex from './components/Pokedex/Pokedex';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+import './PokemonList.css'
+import Pokemon from "../Pokedex/Pokemon"
+function PokemonList () {
+
+    const [pokemonList, setPokemonList] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const POKEDEX_URL = "https://pokeapi.co/api/v2/pokemon"
+
+    async function downloadPokemons(){
+        
+        const response = await axios.get(POKEDEX_URL) // This downloads list of 20 pokemons
+
+        const pokemonResults = response.data.results // we get the array of pokemons from result
+
+        // iterating  over the array of pokemons, and using their url to create an array of promise
+        // that will download those 20 pokemons 
+        const pokemonResultPromise = pokemonResults.map((pokemon)=> axios.get(pokemon.url))
+
+        // Passing that promise array to axios.all
+        const pokemonData = await axios.all(pokemonResultPromise)
+        console.log(pokemonData);
+        const res =  pokemonData.map((pokeData)=> {
+        const pokemon = pokeData.data
+        return (
+           {
+            id: pokemon.id,
+             name: pokemon.name,
+            image: pokemon.sprites.other.dream_world.front_default,
+            type: pokemon.types
+            
+        }
+        )
+        })
+        console.log(res);
+        setPokemonList(res)
+        setIsLoading(false);
+    }
+
+    useEffect(()=>{
+          downloadPokemons()
+    },[])
+
+    return(
+        
+        <div className="pokemon-list-wrapper">
+          <div>Pokemon List</div>
+          {(isLoading) ? 'Loading...' : 
+          pokemonList.map((p)=> <Pokemon name={p.name} image={p.image} key={p.id}/>)
+          } 
+        </div>
+        
+    )
+  
+    
+}
+
+export default PokemonList
